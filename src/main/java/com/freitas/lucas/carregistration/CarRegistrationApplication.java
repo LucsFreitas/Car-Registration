5a57eb4cc91e2447be94a1bb32dd6e61e3922e94
Lucas Freitas <lucas_vbf@hotmail.com>
2020-06-20 10:11:43 -0300

    Inicialização do repositório

---
+++ src/main/java/com/freitas/lucas/carregistration/CarRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.freitas.lucas.carregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRegistrationApplication.class, args);
+	}
+
+}
