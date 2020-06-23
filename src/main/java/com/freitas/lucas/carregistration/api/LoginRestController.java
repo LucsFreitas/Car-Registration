d702c8425f296266fdabba05d2c6e1865d39830c
Lucas Freitas <lucas_vbf@hotmail.com>
2020-06-22 21:58:26 -0300

    Criar endpoint 'post /login'
    Endpoint fake criado para que seja possível realizar o login via swagger. A implementação do processo de login é realizada pelos filtros do Spring Security.

--- src/main/java/com/freitas/lucas/carregistration/api/LoginRestController.java
+++ src/main/java/com/freitas/lucas/carregistration/api/LoginRestController.java
@@ -1,5 +1,6 @@
 package com.freitas.lucas.carregistration.api;
 
+import com.freitas.lucas.carregistration.dto.CredentialsDTO;
 import com.freitas.lucas.carregistration.dto.UserDTO;
 import com.freitas.lucas.carregistration.services.UserService;
 import io.swagger.annotations.ApiOperation;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,11 @@ public class LoginRestController {
     @ApiOperation(value = "Retorna as informações do usuário logado")
     public ResponseEntity<UserDTO> getMe () {
         return new ResponseEntity<>(new UserDTO(this.userService.getCurrentUser()), HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    @ApiOperation(value = "Realiza o login do usuário")
+    public void fakeLogin(@RequestBody CredentialsDTO credentialsDTO) {
+        throw new IllegalStateException("This method shouldn't be called. It's implemented by Spring Security filters.");
     }
 }
